d7b4b188524d32818b3ead529e1a946d4b8afa8b
Joshua Keller <the.cyborganizer@gmail.com>
2021-10-31 10:38:49 -0400

    Implemented some nice QOL improvements:
    - Production logfiles versioned by year
    - Log time of tweet
    - Dryrun mode THAT DEFAULTS TO "TRUE"

--- upload_nightmare.py
+++ upload_nightmare.py
@@ -5,8 +5,12 @@ import random
 from video_tweet import VideoTweet
 from datetime import date, time, datetime, timedelta
 import time as t
+from distutils import util
 
 def upload_tweet(path):
+    global dryRun
+    if (dryRun):
+        return True
     videoTweet = VideoTweet(path)
     videoTweet.upload_init()
     videoTweet.upload_append()
@@ -26,7 +30,6 @@ def generate_upload_times_for_today(num_gifs):
         times.append(start_time + (delta / num_gifs) * i)
 
     delta_minutes = (delta / num_gifs) // timedelta(minutes=1)
-    print(delta_minutes)
     for i in range(len(times)):
         times[i] = times[i] + timedelta(minutes=random.randint(-delta_minutes//2, delta_minutes//2))
     return sorted(times) #just in case
@@ -37,17 +40,32 @@ img_path = config['DEFAULT']['path']
 file_list = sorted(os.listdir(img_path))
 log_file = config['DEFAULT']['log_file']
 
+today = date.today()
+log_file += "." + str(today.year)
+
 start_index = 0
 if len(sys.argv) > 1:
     try:
         start_index = int(sys.argv[1])
     except:
-        print("Usage: python upload_nightmare.py [start index]")
+        print("Usage: python upload_nightmare.py [start index] [dryRun]")
         exit(1)
+
+dryRun = True
+try:
+    dryRunParsed = bool(util.strtobool(sys.argv[2].lower()))
+    dryRun = dryRunParsed
+except:
+    pass
+
+if (dryRun == False):
+    print("Running for real")
+else:
+    print("Running in dryrun mode")
+    log_file += ".dryrun" + str(datetime.now().isoformat())
 
 next_upload = start_index
 
-today = date.today()
 halloween = date(today.year, 10, 31)
 christmas = date(today.year, 12, 25)
 
@@ -72,7 +90,7 @@ while(next_upload <= len(file_list)):
             exit(1)
         else:
             with open(log_file, 'a+') as f:
-                print("Uploaded {}".format(gif_to_upload), file=f)
+                print("Uploaded {} at {}".format(gif_to_upload, str(datetime.now())), file=f)
             next_upload = next_upload+1
     print("See you tomorrow")
     while today == date.today():
